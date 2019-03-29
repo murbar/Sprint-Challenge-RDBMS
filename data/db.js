14267aa67b014e59f2de4ca3538752ea97bd0b90
Joel Bartlett <joel@murbar.com>
2019-03-29 12:32:15 -0400

    Refactor helpers

--- data/db.js
+++ data/db.js
@@ -4,25 +4,28 @@ const knexConfig = require('../knexfile.js');
 
 const db = knex(knexConfig.development);
 
-async function getProjectById(id) {
-  const project = await db('projects')
+const addRecord = async (tableName, record) => {
+  const [newRecordId] = await db(tableName).insert(record);
+  const newRecord = await db(tableName).where({ id: newRecordId });
+  return newRecord;
+};
+
+const getRecordById = async (tableName, id) => {
+  const record = await db(tableName)
     .where({ id })
     .first();
+  return record;
+};
+
+async function getProjectById(id) {
+  const project = await getRecordById('projects', id);
   project.actions = await db('actions').where({ project_id: project.id });
   return project;
 }
 
-async function addProject(project) {
-  const [newProjectId] = await db('projects').insert(project);
-  const newProject = await db('projects').where({ id: newProjectId });
-  return newProject;
-}
+const addProject = async project => addRecord('projects', project);
 
-async function addAction(action) {
-  const [newActionId] = await db('actions').insert(action);
-  const newAction = await db('actions').where({ id: newActionId });
-  return newAction;
-}
+const addAction = async action => addRecord('actions', action);
 
 module.exports = {
   getProjectById,

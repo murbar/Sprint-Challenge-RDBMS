47a54bef71f817c675feaf26d70cf6a78deedcd6
Joel Bartlett <joel@murbar.com>
2019-03-29 11:43:52 -0400

    Build out functions

--- data/db.js
+++ data/db.js
@@ -4,20 +4,28 @@ const knexConfig = require('../knexfile.js');
 
 const db = knex(knexConfig.development);
 
-function addProject(newProject) {
-  return;
-}
-
-function addAction(newAction) {
-  return;
-}
-
 function getProjectById(id) {
-  return;
+  const project = await db('projects')
+    .where({ id })
+    .first();
+  project.actions = await db('actions').where({ project_id: project.id });
+  return project;
+}
+
+function addProject(project) {
+  const [newProjectId] = await db('projects').insert(project);
+  const newProject = await db('projects').where({id: newProjectId})
+  return newProject;
+}
+
+function addAction(action) {
+  const [newActionId] = await db('actions').insert(action);
+  const newAction = await db('actions').where({id: newActionId})
+  return newAction;
 }
 
 module.exports = {
+  getProjectById,
   addProject,
-  addAction,
-  getProjectById
+  addAction
 };

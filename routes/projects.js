418e27fee56542a5081cf449afcc23c01d98acf3
Joel Bartlett <joel@murbar.com>
2019-03-29 13:26:59 -0400

    Tidy up

--- routes/projects.js
+++ routes/projects.js
@@ -38,7 +38,7 @@ router.post('/', async (req, res) => {
       res.status(201).json(newProject);
     }
   } catch (error) {
-    console.log(error);
+    // console.log(error);
     res.status(500).json({ error: 'Cannot create project.' });
   }
 });

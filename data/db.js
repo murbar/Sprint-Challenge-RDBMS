71ab0d6330c80047fc4738f2413aa31b7536e79a
Joel Bartlett <joel@murbar.com>
2019-03-29 13:23:50 -0400

    Refactor helper functions, add exports

--- data/db.js
+++ data/db.js
@@ -3,32 +3,61 @@ const knex = require('knex');
 const knexConfig = require('../knexfile.js');
 
 const db = knex(knexConfig.development);
+const projectsTable = 'projects';
+const actionsTable = 'actions';
 
-const addRecord = async (tableName, record) => {
-  const [newRecordId] = await db(tableName).insert(record);
-  const newRecord = await db(tableName).where({ id: newRecordId });
-  return newRecord;
+const getRecords = tableName => async () => {
+  const records = await db(tableName);
+  return records;
 };
 
-const getRecordById = async (tableName, id) => {
+const getRecordById = tableName => async id => {
   const record = await db(tableName)
     .where({ id })
     .first();
   return record;
 };
 
-async function getProjectById(id) {
+const addRecord = tableName => async record => {
+  const [newRecordId] = await db(tableName).insert(record);
+  const newRecord = await db(tableName).where({ id: newRecordId });
+  return newRecord;
+};
+
+const updateRecord = tableName => async (id, record) => {
+  const updatedCount = await db(tableName)
+    .where({ id })
+    .update(record);
+  return updatedCount;
+};
+
+const deleteRecord = tableName => async id => {
+  const deletedCount = await db(tableName)
+    .where({ id })
+    .del();
+  console.log(deletedCount);
+  return deletedCount;
+};
+
+const getProjectById = async id => {
   const project = await getRecordById('projects', id);
   project.actions = await db('actions').where({ project_id: project.id });
   return project;
-}
-
-const addProject = async project => addRecord('projects', project);
-
-const addAction = async action => addRecord('actions', action);
+};
 
 module.exports = {
-  getProjectById,
-  addProject,
-  addAction
+  projects: {
+    getAll: getRecords(projectsTable),
+    getById: getProjectById,
+    create: addRecord(projectsTable),
+    update: updateRecord(projectsTable),
+    del: deleteRecord(projectsTable)
+  },
+  actions: {
+    getAll: getRecords(actionsTable),
+    getById: getRecordById(actionsTable),
+    create: addRecord(actionsTable),
+    update: updateRecord(actionsTable),
+    del: deleteRecord(actionsTable)
+  }
 };
